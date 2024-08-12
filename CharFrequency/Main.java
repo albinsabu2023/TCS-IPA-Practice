b0c1b7c1a5dbe396febe090ec49f86cee2bc0864
albinsabu2023 <albinsullas2023@gmail.com>
2024-08-12 23:46:11 +0530

    char frequency

---
+++ CharFrequency/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Character, Integer> mp = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        for (Character ch : str.toCharArray()) {
+            if (mp.containsKey(ch)) {
+                mp.put(ch, mp.get(ch) + 1);
+            } else {
+                mp.put(ch, 1);
+            }
+        }
+        sc.close();
+        // iterating over the map
+        for (Map.Entry<Character, Integer> entry : mp.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
